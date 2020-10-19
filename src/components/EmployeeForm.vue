1ca0dac750382294bc347d4d9998262df31f43bd
dawneraq <dawneraq@gmail.com>
2020-10-19 16:59:32 -0700

    validate form before submitting

--- src/components/EmployeeForm.vue
+++ src/components/EmployeeForm.vue
@@ -2,12 +2,27 @@
   <form id="employee-form" @submit.prevent="handleSubmit">
     <label>
       Employee name
-      <input v-model="employee.name" type="text" />
+      <input
+        type="text"
+        v-model="employee.name"
+        :class="{ 'has-error': submitting && invalidName }"
+        @focus="clearStatus"
+        @keypress="clearStatus"
+      />
     </label>
     <label>
       Employee email
-      <input v-model="employee.email" type="text" />
+      <input
+        type="text"
+        v-model="employee.email"
+        :class="{ 'has-error': submitting && invalidEmail }"
+        @focus="clearStatus"
+      />
     </label>
+    <p v-if="error && submitting" class="error-message">
+      ❗ Please fill out all required fields
+    </p>
+    <p v-if="success" class="success-message">✅ Employee successfully added</p>
     <button>Add employee</button>
   </form>
 </template>
@@ -17,12 +32,39 @@ export default {
   name: 'employee-form',
   data() {
     return {
+      submitting: false,
+      error: false,
+      success: false,
       employee: { name: '', email: '' },
     };
   },
+  computed: {
+    invalidName() {
+      return this.employee.name === '';
+    },
+    invalidEmail() {
+      return this.employee.email === '';
+    },
+  },
   methods: {
     handleSubmit() {
+      this.submitting = true;
+      this.clearStatus;
+
+      if (this.invalidName || this.invalidEmail) {
+        this.error = true;
+        return;
+      }
+
       this.$emit('add-employee', this.employee);
+      this.employee = { name: '', email: '' };
+      this.error = false;
+      this.success = true;
+      this.submitting = false;
+    },
+    clearStatus() {
+      this.success = false;
+      this.error = false;
     },
   },
 };
@@ -31,5 +73,17 @@ export default {
 <style scoped>
 form {
   margin-bottom: 2rem;
+}
+
+[class*='-message'] {
+  font-weight: 500;
+}
+
+.error-message {
+  color: #d33c40;
+}
+
+.success-message {
+  color: #32a95d;
 }
 </style>
