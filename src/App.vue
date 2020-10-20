cb9646dc457e6a8648dd36a4642a7fca90c1682b
dawneraq <dawneraq@gmail.com>
2020-10-19 17:33:18 -0700

    add new employee via POST

--- src/App.vue
+++ src/App.vue
@@ -15,6 +15,8 @@
 import EmployeeTable from '@/components/EmployeeTable';
 import EmployeeForm from '@/components/EmployeeForm';
 
+const API_URL = 'https://jsonplaceholder.typicode.com/users';
+
 export default {
   name: 'app',
   components: {
@@ -30,15 +32,15 @@ export default {
     this.getEmployees();
   },
   methods: {
-    addEmployee(employee) {
-      const lastId =
-        this.employees.length > 0
-          ? this.employees[this.employees.length - 1].id
-          : 0;
-      const id = lastId + 1;
-      const newEmployee = { ...employee, id };
+    async addEmployee(employee) {
+      const response = await fetch(API_URL, {
+        method: 'POST',
+        body: JSON.stringify(employee),
+        headers: { 'Content-type': 'application/json; charset=UTF-8' },
+      });
+      const data = await response.json();
 
-      this.employees = [...this.employees, newEmployee];
+      this.employees = [...this.employees, data];
     },
     deleteEmployee(id) {
       this.employees = this.employees.filter((employee) => employee.id !== id);
@@ -50,9 +52,7 @@ export default {
     },
     async getEmployees() {
       try {
-        const response = await fetch(
-          'https://jsonplaceholder.typicode.com/users'
-        );
+        const response = await fetch(API_URL);
         const data = await response.json();
 
         this.employees = data;
